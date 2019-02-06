01a21551432b7b36d2d6f8922c30ffbe084999ea
wkd3475 <wkd3475@gail.com>
2019-02-06 23:32:17 +0900

    complete

--- 이항계수/b_11050/b_11050.c
+++ 이항계수/b_11050/b_11050.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+int factorial(int n);
+
 int main() {
-    
+    int n, k, result;
+    scanf("%d %d", &n, &k);
+    result = factorial(n) / factorial(n-k) / factorial(k);
+    printf("%d", result);
     return 0;
+}
+
+int factorial(int n) {
+    if(n==1 || n==0) { //factorial(0)은 1이다.
+        return 1;
+    }
+    else {
+        int result = n * factorial(n-1);
+        return result;
+    }
 }
